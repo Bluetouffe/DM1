ca4155a6d4aa8b332cb52268203dba0f84740dce
Bluetouffe <louisbonicel@gmail.com>
2015-01-26 13:09:06 +0100

    Update fonctions.h

--- Headers/fonctions.h
+++ Headers/fonctions.h
@@ -3,46 +3,48 @@
 
 #define DECALAGE 5
 #define TAILLE_POLYNOME 1024
+#define DEBUG
 
 typedef struct monome{
     float coeff;
     int exposant;
 }monome;
 
-char menu();
-void printChoice();
+char menu(void);
+void printChoice(void);
 int valideMenu(char p);
 
-void exercice1();
-void crypt(char *p);
+void exercice1(void);
+void crypt(char* p);
 
-void question1();
-void saisie(monome * p, int *taille);
-monome saisieMonome();
-void saisiePolynome(monome * Polynome, int * i);
+void question1(void);
+void saisie(monome* p, int* taille);
+monome saisieMonome(void);
+void saisiePolynome(monome* Polynome, int* i);
 
-void question2();
-void suppressionDoublons(monome * polynome, int * taillePolynome);
+void question2(void);
+void suppressionDoublons(monome* polynome, int* taillePolynome);
 
-void question3();
-void triPolynome(monome * P, int start, int stop);
-int estPresentDans(monome toTest, monome * polynome, int taillePolynome);
-int estTrie(monome * P, int start, int stop);
-int placerPivot(monome * P, int start, int stop);
+void question3(void);
+void triPolynome(monome* P, int start, int stop);
+int estPresentDans(monome toTest, monome* polynome, int taillePolynome);
+int estTrie(monome* P, int start, int stop);
+int placerPivot(monome* P, int start, int stop);
 
-void question4();
+void question4(void);
 void displayMonome(monome m);
 void displayFirstMonome(monome m);
 void displayPolynome(monome * P, int tailleP);
 
-void question5();
-void ajouterTaillePolynome(monome * P, int i);
-double eval(monome * polynome, double x);
+void question5(void);
+void ajouterTaillePolynome(monome* P, int i);
+double eval(monome* polynome, double x);
 
 void concatenerPolynome(char* chaine, char* out);
-monome extrairePremierMonome(char* chaine);
-//monome * entrezPolynome();
-void entrezPolynome();
+monome premierMonome(char* chaine);
+void entrezPolynome(void);
+int estUnMonomeValide(char* chaine);
+int nbrOccChar(char* chaine, char c);
 void estUnExposant(char* string, int* degres);
 int estUnNombre(char c);
 int estUnSep(char c);
